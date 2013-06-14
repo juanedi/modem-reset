d880d369a3abd9b472856d40a456cb609c8ae028
Juan Edi <juanedi@zauberlabs.com>
2013-06-13 21:35:24 -0300

    Ordeno un poco

--- modem-reset.js
+++ modem-reset.js
@@ -1,11 +1,18 @@
 var page = require('webpage').create();
 
+/**
+ * Datos a completar! 
+ *		- Login a la interfaz de configuracion del modem
+ *		- Path absoluto al archivo de backup
+ */
 page.settings.userName = "";
 page.settings.password = "";
+backup_path = "";
+
 
 modem_ui = "http://192.168.0.1/RgBackupRestore.asp";
 jquery_src = "http://code.jquery.com/jquery-1.9.1.min.js";
-backup_path = "/Users/jedi/Desktop/GatewaySettings.bin";
+
 
 page.onConsoleMessage = function (msg) {
     console.log(msg);
@@ -21,13 +28,12 @@ page.open(modem_ui, function (status) {
 
 		page.includeJs(jquery_src, function() {
 			page.evaluate(function() {
-
 				jQuery.noConflict();
 				var filePath = jQuery("input[name=ImportFile]").val();
+				
 				if (filePath) {
 					console.log("Reiniciando modem!");
 					jQuery("input[type=submit]").click();
-
 				} else {
 					console.log("Error al subir archivo, comprobar el path.")
 				}
