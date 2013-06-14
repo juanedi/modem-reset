b819a1d8cb552aa8028b58af8e64c60376cc2ca5
Juan Edi <juanedi@zauberlabs.com>
2013-06-13 22:35:37 -0300

    saco dependencia jquery (no funciona el submit)

--- modem-reset.js
+++ modem-reset.js
@@ -1,8 +1,8 @@
 var page = require('webpage').create();
 
 /**
- * Este script ingresa via phantomjs a la interfaz web de
- * configuración del modem Technicolor TC7110.
+ * Este script ingresa via phantomjs a la interfaz web de configuración
+ * del modem Technicolor TC7110.
  *
  * Dado que el modem no ofrece interfaz telnet, se debe simular el
  * acceso desde un browser.
@@ -13,19 +13,15 @@ var page = require('webpage').create();
  * que se reinicie el modem y se restrablezca la conexión en caso de que
  * esté caída.
  *
- *
- * Datos a completar:
- *		- Login a la interfaz de configuracion del modem
- *		- Path absoluto al archivo de backup
- *
  */
+
+// datos de login a la UI del modem
 page.settings.userName = "";
 page.settings.password = "";
+
+// path absoluto al archivo de backup
 backup_path = "";
-
-
 modem_ui = "http://192.168.0.1/RgBackupRestore.asp";
-jquery_src = "http://code.jquery.com/jquery-1.9.1.min.js";
 
 
 page.onConsoleMessage = function (msg) {
@@ -33,32 +29,36 @@ page.onConsoleMessage = function (msg) {
 };
 
 page.open(modem_ui, function (status) {
-
-
 	if (status === "success") {
 
 		console.log("Seleccionando backup a subir: " + backup_path);
 		page.uploadFile('input[name=ImportFile]', backup_path);
+		page.evaluate(function() {
+			// Dentro del contexto del sitio.
 
-		page.includeJs(jquery_src, function() {
-			page.evaluate(function() {
-				jQuery.noConflict();
-				var filePath = jQuery("input[name=ImportFile]").val();
-
-				if (filePath) {
-					console.log("Reiniciando modem!");
-					jQuery("input[type=submit]").click();
-				} else {
-					console.log("Error al subir archivo, comprobar el path.")
+			var fileInput = document.getElementsByName("ImportFile")[0];
+			var submitInput = (function() {
+				allInputs = document.getElementsByTagName("input");
+				for(i = 0; i < allInputs.length; i++) {
+				  if (allInputs[i].value === "Restore") return allInputs[i];
 				}
+			})();
 
-			});
-			console.log("Fin!");
-			phantom.exit();
+			if (fileInput.value) {
+				console.log("Reiniciando modem!");
+
+				// FIXME: No hace click :-(
+				submitInput.click();
+			} else {
+				console.log("Error al subir archivo, comprobar el path.")
+			}
+
 		});
+		console.log("Fin!");
+		phantom.exit();		
+
 	} else {
 		console.log("Error al cargar la página...")
 		phantom.exit();
 	}
-
 });
